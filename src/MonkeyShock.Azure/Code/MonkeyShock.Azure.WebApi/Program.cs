37814bb1dd52089229a8f55871b559367a46cd1f
Piotr Gaszewski <piog@netcompany.com>
2021-11-22 16:47:17 +0100

    Removing swagger

--- src/MonkeyShock.Azure/Code/MonkeyShock.Azure.WebApi/Program.cs
+++ src/MonkeyShock.Azure/Code/MonkeyShock.Azure.WebApi/Program.cs
@@ -20,19 +20,8 @@ builder.Services.AddDbContext<CountryAppContext>(
     options => options.UseSqlServer(builder.Configuration.GetConnectionString("Default"))
 );
 builder.Services.AddControllers().AddOData(opt => opt.AddRouteComponents("v1", GetEdmModel()).Filter().Select().Expand());
-builder.Services.AddSwaggerGen(c =>
-{
-    c.SwaggerDoc("v1", new() { Title = "ODataTutorial", Version = "v1" });
-});
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "ODataTutorial v1"));
-}
 
 app.UseHttpsRedirection();
 
